a05eaa8ca543acb7147e92dba0da4e645bee45bb
Sei malvagio <145441542+Sei-malvagio@users.noreply.github.com>
2024-03-27 22:56:19 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 function Home() 
 {
   return (
-    <div className="flex flex-col jutify-center items-center space-y-2">
-      <h1 className="text-center text-lg font-semibold">
+    <div className="flex flex-col justify-center items-center space-y-4">
+      <h1 className="text-center text-xl2 font-semibold">
     MedBox Company inc.
     </h1>
-      <a href="https://wa.me/6281387278648" className="px-4 py-1 text-purple-600 font-semibold text-sm border border-purple-500 rounded-lg hover:bg-purple-600 text-whte">Chat Kami</a>
+      <a href="https://wa.me/6281387278648" className="px-4 py-1 text-purple-600 font-semibold text-sm border border-purple-500 rounded-lg hover:bg-purple-600 text-white">Chat Kami</a>
     </div>
     )
 }
