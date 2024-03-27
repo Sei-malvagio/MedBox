244fd975cfb08d240153a5d80c59649703d1495a
Sei malvagio <145441542+Sei-malvagio@users.noreply.github.com>
2024-03-27 22:25:52 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-export default function Home() 
+function Home() 
 {
-  return {
+  return (
     <div>
       <h1 class="text-center">
     MedBox Company inc.
     </h1>
     </div>
-      }
-      }
+    )
+}
+
+export default Home
