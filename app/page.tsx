219da837a6de1b013c8a142a95fddbe53b72f565
Daffabot <daffabot@programmer.net>
2024-04-26 14:37:27 +0700

    base sudah jadi page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -89,6 +89,35 @@ function Home()
       </section>
     </main>
   </div>
+  <script>
+    window.onload = () => {
+      const animatedElement = document.getElementById("animated");
+      const anima = document.getElementById("anime");
+
+      animatedElement.addEventListener("animationend", () => {
+        animatedElement.removeAttribute("id");
+        anima.removeAttribute("id");
+      });
+
+      window.addEventListener("scroll", setScrollVar);
+      window.addEventListener("resize", setScrollVar);
+
+      function setScrollVar() {
+        const htmlElement = document.body;
+        const percentOfScreenHeightScrolled = htmlElement.scrollTop / htmlElement.clientHeight;
+        const au = percentOfScreenHeightScrolled * 100;
+        console.log("hasil: " + au);
+        // console.log(1 - (1 / 100) * Number(Math.min(au, 30)) * 100 / 30);
+        // console.log("jika berada di bawah: " + (1 / 100) * (Math.max(au, 25) - 25) * 100 / 75);
+        htmlElement.style.setProperty(
+          "--scroll",
+          au
+        );
+      }
+
+      setScrollVar()
+    }
+  </script>
     )
 }
 
