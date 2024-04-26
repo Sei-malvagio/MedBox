a1db6031501c6bf78b3e23965d1bdc65042d48ec
Sei malvagio <145441542+Sei-malvagio@users.noreply.github.com>
2024-04-26 15:14:26 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 function Home() 
 {
   return (
-    <>
     <div className="block">      
       <div className="min-h-screen flex flex-col justify-center brah">
     <nav className="fixed top-0 z-10 sm:z-[1] h-14 w-full flex justify-between transition duration-500 ease-in-out" :className="{'max-[640px]:h-full': open, 'bg-nav': open }" x-data="{ open: false }">
@@ -116,7 +115,6 @@ function Home()
     }
   </script>
       </div>
-      </>
     )
 }
 
